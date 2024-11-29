104c4820782d5ca916cafe3cdd8063bd354ba20c
Alvin Francisco Prado Peynado <alvinprado185@gmail.com>
2024-11-29 03:17:34 -0600

    primercommit.py

---
+++ Tallerpy.py
@@ -0,0 +1,75 @@
+cadena1="Me llamo"
+cadena2="Jose"
+cadena_concatenada=cadena1+cadena2
+print(cadena1)
+print(cadena2)
+print(cadena_concatenada)
+"--------------------------------------------"
+nombre:"Daniel"
+variable_concatenada=f"Hola {nombre} bienvenido a Churrumais."
+print(f"Hola{nombre}bienvenido al curso")
+"------------------------------------------"
+d1=54
+d2=4
+print(d1)
+print(d2)
+"----------------------------------------"
+r=d1-d2
+print(r)
+
+"------------------------------------------"
+v=input("Hola,introduce un texto")
+print(v)
+vn=input("Ingresa un valor numerico")
+print(int(vn))
+print(type(vn))
+"---------------------------------------------------"
+num=int(input("Ingresa n valor para este caso"))
+if num>10:
+    print("Es tremendo viejo")
+else:
+    print("Todavia nacio ayer")
+"------------------------------------------------------"
+n=int(input("Porfavor de ingresar un numero"))
+if n % 2 == 0 and n > 10:
+    print(f"El numero {n} es un par y mayor que 10")
+elif n % 2 == 0:
+    print(f"El numero {n} es un par pero no es mayor que 10")
+else:
+    print(f"El numero {n} no da como resultado un par")
+
+"-------------------------------------------------------------"
+
+for i in range(5):
+    print("Python")
+lim=int(input("Añade un limite"))
+i=1
+
+while i<= lim:
+    print(i)
+    i+1=1
+
+lim = int(input("ingresa el periodo de limite"))
+i=1
+while i <= lim:
+    if i%2==0
+    print (i)
+    i+1=1
+"-----------------------------------------------------------------------"
+lista=[50,60,70,80,90]
+print (lista)
+print("Quien se lleva el 3 lugar es:",lista[2])
+
+triple=(5,6,7,8,9)
+print(triple)
+print("Lugar n. 4:",triple[3] )
+
+tamaulipas={100,150,200,250,300}
+print(tamaulipas)
+print("Lugar n. 2:", "No se encuentra en servicio")
+
+listan=["Lopez", "Ricardo", "Montoya", "Kinkong", "Eula",]
+print(listan)
+print("Lugar n. 1:",listan[0])
+print("Lugar n. 2:",listan[4])
+"-------------------------------------------------------------------"
